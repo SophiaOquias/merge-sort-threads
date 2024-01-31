6871f6e7acf4363c236cae044cd65a97640d41bd
Sophia <sophia.oquias@gmail.com>
2024-01-31 19:56:20 +0800

    queue error

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,10 +12,11 @@
 using namespace std;
 
 typedef pair<int, int> ii;
-
 int n; 
 std::queue<ii> task_queue;
 std::mutex task_mutex;
+
+bool flag = true; 
 
 /*
 This function generates all the intervals for merge sort iteratively, given the
@@ -39,28 +40,25 @@ e     : int         - end index (inclusive) of merge
 */
 void merge(vector<int>& array, int s, int e);
 
-void thread_merge(vector<int>& array, vector<std::mutex> &locks) {
-    while (!task_queue.empty()) {
+void print_array(vector<int>& array);
 
+void thread_merge(vector<int>& array) {
+    while (flag) {
         std::unique_lock<std::mutex> lock(task_mutex);
-        if (task_queue.empty()) {
-            break; 
+        if (!task_queue.empty()) {
+            ii temp = task_queue.front();
+            task_queue.pop();
+            lock.unlock();
+
+            int s = temp.first;
+            int e = temp.second;
+
+            if (s < 0 || e < 0) {
+                continue;
+            }
+
+            merge(array, s, e);
         }
-        ii temp = task_queue.front();
-        task_queue.pop(); 
-        lock.unlock(); 
-
-        int s = temp.first; 
-        int e = temp.second; 
-
-        vector<std::unique_lock<std::mutex>> lockArray;
-
-        // aquire locks in range 
-        for (int i = s; i <= e; i++) {
-            lockArray.emplace_back(locks[i]);
-        } 
-
-        merge(array, s, e);
     }
 }
 
@@ -69,42 +67,64 @@ int main() {
     std::mt19937 rng(42); 
 
     // TODO: Get array size and thread count from user
-    n = 15; 
-    int thread_count = 30; 
+    n = 10000000; 
+    int thread_count = 10; 
     std::uniform_int_distribution<int> dist(1, n);
 
     // TODO: Generate a random array of given size
     std::vector<int> array(n); 
     for (int &num : array) {
         num = dist(rng);
-        std::cout << num << ", ";
     }
-    std::cout << "" << std::endl; 
 
     // TODO: Call the generate_intervals method to generate the merge sequence
     vector<ii> intervals = generate_intervals(0, n - 1); 
-    for (ii& interval : intervals) {
-        task_queue.push(interval); 
-    }
 
     // TODO: Call merge on each interval in sequence
-    std::vector<std::mutex> mutex_array(n);
 
     // Once you get the single-threaded version to work, it's time to implement 
     // the concurrent version. Good luck :)
     std::vector<std::thread> threads;
 
     for (int i = 0; i < thread_count; i++) {
-        threads.emplace_back(thread_merge, std::ref(array), std::ref(mutex_array));
+        threads.emplace_back(thread_merge, std::ref(array));
     }
+
+    int max_depth = static_cast<int>(std::floor(std::log2(n))) + 1; // depth = log_2(n) + 1
+    int arr_ptr = 0; 
+
+    for (int k = max_depth + 1; k > 0; k--) {
+        int num_leaves = static_cast<int>(pow(2, k - 1));
+
+        if (k == max_depth + 1 && n != num_leaves) {
+            int d = num_leaves - n; 
+            num_leaves = n - d; 
+        }
+        
+        int start = arr_ptr; 
+        for (int i = arr_ptr; i < start + num_leaves; i++) {
+            task_queue.push(intervals[arr_ptr]); 
+            arr_ptr++;
+        }
+
+        while (!task_queue.empty()) { }
+    }
+
+    flag = false; 
 
     for (auto& thread : threads) {
         thread.join();
     }
 
+    bool isSorted = is_sorted(array.begin(), array.end());
+    cout << "Array is " << (isSorted ? "sorted" : "not sorted") << endl;
+}
+
+void print_array(vector<int>& array) {
     for (int& element : array) {
         std::cout << element << ", ";
     }
+
     std::cout << "" << std::endl;
 }
 
